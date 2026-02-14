5631894b44106d7a67e2b64a8733db60707fefac
parv-306 <parvjain835@gmail.com>
2026-02-14 10:33:56 +0000

    a

---
+++ if elsee.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main() {
+    int marks1, marks2, marks3;
+    printf("enter  the marks1\n");
+    scanf("%d", &marks1);
+    printf("enter  the marks2\n");
+    scanf("%d", &marks2);
+    printf("enter  the marks3\n");
+    scanf("%d", &marks3);
+
+    if(marks1 <33 || marks2 <33 || marks3<33) {
+        printf("you are fail in individual subject");
+
+    
+
+    }
+    else if((marks1 + marks2 + marks3)/3 < 40) {
+        printf("you are failed due to less marks");
+
+    }
+    else {
+        printf("you are pass");
+    }
+    
+    
+
+    
+    return 0;
+
+}
